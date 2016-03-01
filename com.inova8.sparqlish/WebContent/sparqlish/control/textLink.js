4448b1ce1e73dd9c34e41ec3625a78230155f6d6
Peter <peter.lawrence@inova8.com>
2016-03-01 18:07:30 -0500

    Added InNewTab to linkText to invoke link in a new tab rather than replace current

--- com.inova8.sparqlish/WebContent/sparqlish/control/textLink.js
+++ com.inova8.sparqlish/WebContent/sparqlish/control/textLink.js
@@ -18,6 +18,10 @@ sap.ui.define([ "sap/ui/core/Control", "sap/m/Link", "sap/m/Text" ], function(Co
 				tooltip : {
 					type : "string",
 					defaultValue : ""
+				},
+				inNewTab : {
+					type : "boolean",
+					defaultValue : true
 				}
 			},
 			aggregations : {
@@ -35,18 +39,26 @@ sap.ui.define([ "sap/ui/core/Control", "sap/m/Link", "sap/m/Text" ], function(Co
 		},
 		init : function() {
 			this.setAggregation("_text", new sap.m.Text({}).addStyleClass("resultValue"));
-			this.setAggregation("_link", new sap.m.Link({}).addStyleClass("resultValue"));
+			this.setAggregation("_link", new sap.m.Link({}).addStyleClass("resultValue").attachPress(function(oEvent) {
+				if (oEvent.getSource().getParent().getInNewTab()) {
+					oEvent.cancelBubble();
+					oEvent.preventDefault();
+					sap.m.URLHelper.redirect(oEvent.getSource().getHref(), true);
+				} 
+			}));
 		},
 		renderer : function(oRM, oControl) {
 			oRM.write("<div");
 			oRM.writeControlData(oControl);
 			oRM.writeClasses();
 			oRM.write(">");
-			var sValue = oControl.getProperty("value");
+			var sValue = oControl.getValue();
+			// Test
+//			 oRM.renderControl(oControl.getAggregation("_link").setText(oControl.getLinkText()).setHref("http://www.inova8.com"));
 			if (!utils.validateUrl(sValue)) {
 				oRM.renderControl(oControl.getAggregation("_text").setText(sValue));
 			} else {
-				oRM.renderControl(oControl.getAggregation("_link").setText(oControl.getProperty("linkText")).setHref(sValue));
+				oRM.renderControl(oControl.getAggregation("_link").setText(oControl.getLinkText()).setHref(sValue));
 			}
 			oRM.write("</div>")
 		}
